ddfa203fca5e511051343f2cea021cdff97cf4fc
KhengChamnan <chamnan.kheng@student.cadt.edu.kh>
2025-04-04 22:43:50 +0700

    1.1

--- android/gradle/app/src/main/java/com/example/tourism_app/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.tourism_app;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-    // Uses Flutter v2 embedding by default - no additional code needed
-} 
